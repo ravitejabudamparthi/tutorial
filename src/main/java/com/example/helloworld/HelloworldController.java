45574a35d10b5e12ac406244af3660eb3ab46553
raviteja <ravitejabudamparthi@gmail.com>
2024-10-22 08:44:26 +0000

    commitsolution   git commit -M main git push -u origin main
    git commit -m commitsolutio

--- src/main/java/com/example/helloworld/HelloworldController.java
+++ src/main/java/com/example/helloworld/HelloworldController.java
@@ -1,22 +1,23 @@
 package com.example.helloworld;
 
-
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @RestController
-public class hellowordController {
-
+public class HelloworldController {
 
     @GetMapping("/")
     public String getWelcomeText() {
         return "Hello World";
     }
 
-
-    @GetMapping("/name")
-    public String getName() {
+    @GetMapping("/name1")
+    public String getName1() {
         return "Ravi teja";
+    }
+
+    @GetMapping("/name2")
+    public String getName2() {
+        return "Raviteja";
     }
 }
